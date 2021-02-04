4a3505bb6f568e8136faf5a31c76cbb4e46de4f2
Danyyal Ali Yar <danyyal.ali@techverx.com>
2021-02-04 15:58:11 +0500

    init

---
+++ Collections/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace Collections
+{
+    class Program
+    {
+        public int age = 10;
+        public string name = "Ali";
+        public float cgpa = 3.3f;
+        public double sgpa = 3.66;
+        public char section = 'E';
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //abc
+            Program pg = new Program();
+            ArrayList arrayone = new ArrayList();
+            Console.WriteLine(arrayone);
+            arrayone.Add(pg.age);
+            arrayone.Add(pg.name);
+            arrayone.Add(pg.cgpa);
+            arrayone.Add(pg.sgpa);
+            arrayone.Add(pg.section);
+            Console.WriteLine(arrayone);
+
+
+
+        }
+    }
+}
